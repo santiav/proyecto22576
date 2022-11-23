ae555a297e8c2e108e5ccef4d36e5964c721fd41
Santi Acosta Verrier <santiav@gmail.com>
2022-11-23 18:19:19 -0300

    index y about pasados a hbs, parciales creados

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 
 const express = require('express')
 const app = express()
+const hbs = require('hbs');
 
-app.use(express.static('public'));
+app.set('view engine', 'hbs'); // seteo motor de plantilla 
+hbs.registerPartials(__dirname + '/views/partials'); // Indico donde se encuentran los "parciales"
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
+app.use(express.static('public')); // Tomar los archivos de public
+
+
+// Ruta RAÍZ (index)
+app.get('/', function (req, res) {
+   res.render('index', {
+        titulo: "Mi página web"
+
+   })
+
+})
+
+// About (sobre nosotros)
+app.get('/about', function (req, res) {
+    res.render('about', {
+         titulo: "Sobre nosotros"
+         
+    })
+ 
+ })
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
