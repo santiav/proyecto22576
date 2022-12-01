b3e4f0e35a2754220597df6260831642488a4910
Santi Acosta Verrier <santiav@gmail.com>
2022-12-01 11:47:03 -0300

    parte 2 - usando plantilla inicial

--- server.js
+++ server.js
@@ -1,32 +1,53 @@
-
 const express = require('express')
 const app = express()
 const hbs = require('hbs');
+const productos = require('./data/products.json')
 
-app.set('view engine', 'hbs'); // seteo motor de plantilla 
-hbs.registerPartials(__dirname + '/views/partials'); // Indico donde se encuentran los "parciales"
+const port = 3000
 
-app.use(express.static('public')); // Tomar los archivos de public
+app.set('view engine', 'hbs'); // indica que usaremos la plantilla "handlebars"
+hbs.registerPartials(__dirname + '/views/partials'); // indicamos el directorio hacia los parciales
+
+app.use(express.static('public')); // indica la carpeta public para considerar archivos estáticos
 
 
-// Ruta RAÍZ (index)
-app.get('/', function (req, res) {
-   res.render('index', {
-        titulo: "Mi página web"
 
-   })
+// Cuando voy a la raiz, entonces se ejecutan las siguientes instrucciones, por ejemplo, responde con renderizar el archivo index, que se encuentra en la carpeta views.
+app.get('/', (req, res) => {
+  // (indica el nombre del archivo hbs, objeto)
+  res.render('index', {
+    titulo: "Mi página web",
+    productos: productos.products
+  })
 
 })
 
-// About (sobre nosotros)
-app.get('/about', function (req, res) {
-    res.render('about', {
-         titulo: "Sobre nosotros"
-         
-    })
- 
- })
+app.get('/como-comprar', (req, res) => {
+  res.render('como-comprar', {
+    titulo: "Cómo comprar"
+  })
+})
 
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+app.get('/contacto', (req, res) => {
+  res.render('contacto', {
+    titulo: "Contacto"
+  })
+})
+
+app.get('/producto-detalle', (req, res) => {
+  res.render('producto-detalle', {
+    titulo: "Detalle del producto"
+  })
+})
+
+app.get('/sobre-nosotros', (req, res) => {
+  res.render('sobre-nosotros', {
+    titulo: "Sobre nosotros"
+  })
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
